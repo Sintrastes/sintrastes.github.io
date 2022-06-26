d9f9b77a3758ae926ca54a0c61b09ef5b5c6d782
Nathan Bedell <nbedell@tulane.edu>
2022-06-26 11:52:26 -0400

    Add debugging statement to javascript functions.

--- demos/xen_fret/main.js
+++ demos/xen_fret/main.js
@@ -10,24 +10,38 @@ function download(text, name, type) {
 }
 
 function setCookie(cname, cvalue, exdays) {
+    console.log("setCookie 1");
     const d = new Date();
+    console.log("setCookie 1");
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    console.log("setCookie 1");
     let expires = "expires="+ d.toUTCString();
+    console.log("setCookie 1");
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    console.log("setCookie 1");
 }
 
 function getCookie(cname) {
+    console.log("setCookie 1");
     let name = cname + "=";
+    console.log("setCookie 2");
     let decodedCookie = decodeURIComponent(document.cookie);
+    console.log("setCookie 3");
     let ca = decodedCookie.split(';');
+    console.log("setCookie 4");
     for(let i = 0; i <ca.length; i++) {
+      console.log("setCookie loop 1");
       let c = ca[i];
+      console.log("setCookie loop 2");
       while (c.charAt(0) == ' ') {
+        console.log("setCookie loop 3");
         c = c.substring(1);
       }
       if (c.indexOf(name) == 0) {
+        console.log("setCookie loop 4");
         return c.substring(name.length, c.length);
       }
     }
+    console.log("setCookie 5");
     return "";
 }
