c674c931da0f063ae5473bac036a2d852587343b
Nathan Bedell <nbedell@tulane.edu>
2022-07-04 17:37:39 -0400

    Update terminal script.

--- demos/hafly_repl/main.js
+++ demos/hafly_repl/main.js
@@ -1,11 +1,14 @@
 
 function initTerm() {
-    setTimeout(function(){ 
+    setTimeout(function() { 
         var term = new Terminal();
         term.open(document.getElementById('terminal'));
-        term.write(' >');
+        term.write(' > ');
         term.onKey(function(x) {
             term.write(x.key);
         });
+        term.onLineFeed(function() {
+            term.write('\n > ')
+        })
     }, 150);
 }
