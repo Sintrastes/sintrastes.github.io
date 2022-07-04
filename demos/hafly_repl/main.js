68185fcc18a546ab200cca5e647a659b6688a1ec
Nathan Bedell <nbedell@tulane.edu>
2022-07-04 18:46:29 -0400

    Update main.js

--- demos/hafly_repl/main.js
+++ demos/hafly_repl/main.js
@@ -1,14 +1,18 @@
 
-function initTerm() {
+function initTerm(callback) {
     setTimeout(function() { 
         var term = new Terminal({convertEol: true});
+        var currentText = '';
         term.open(document.getElementById('terminal'));
         term.write(' > ');
         term.onKey(function(x) {
             if (x.key != '\r') {
                 term.write(x.key);
             } else {
-                term.write('\r\n > ');
+                callback(currentText, function(processedText) {
+                    term.write(processedText);
+                    term.write('\r\n > ');
+                });
             }
         });
     }, 150);
