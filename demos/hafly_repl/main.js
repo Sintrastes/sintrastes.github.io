0da44a562bc15b6b4835c85a0cb80acd93241419
Nathan Bedell <nbedell@tulane.edu>
2023-02-06 21:18:18 -0700

    Add better arrow handling.

--- demos/hafly_repl/main.js
+++ demos/hafly_repl/main.js
@@ -2,6 +2,8 @@ function initTerm(callback) {
     setTimeout(function() { 
         var term = new Terminal({convertEol: true});
         var currentText = '';
+        var cursorPosition = 0;
+
         term.open(document.getElementById('terminal'));
         term.write(' > ');
         term.onKey(function(x) {
@@ -17,9 +19,11 @@ function initTerm(callback) {
                     term.write("\b \b");
                     currentText = currentText.substring(0, currentText.length - 1);
                 }
-            } else if (x.key != '\x1B[A' && x.key != '\x1B[B') {
+            } else if (x.key != '\x1B[A' && x.key != '\x1B[B' && x.key != '\x1B[C' && x.key != '\x1B[D') {
                 term.write(x.key);
                 currentText += x.key;
+            } else if (x.key == '\x1B[D' && cursorPosition > currentText.length) {
+                term.write(x.key);
             }
             console.log(x);
         });
