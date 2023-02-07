ae1f2ca36c355c8cd9a6b9912dc95e9550bc9830
Nathan Bedell <nbedell@tulane.edu>
2023-02-06 21:21:54 -0700

    Tweak script.

--- demos/hafly_repl/main.js
+++ demos/hafly_repl/main.js
@@ -1,5 +1,6 @@
+
 function initTerm(callback) {
-    setTimeout(function() { 
+    // setTimeout(function() { 
         var term = new Terminal({convertEol: true});
         var currentText = '';
         var cursorPosition = 0;
@@ -13,19 +14,23 @@ function initTerm(callback) {
                     term.write(processedText);
                     term.write('\r\n > ');
                     currentText = '';
+                    cursorPosition = 0;
                 });
             } else if (x.key.charCodeAt(0) == 127) {
                 if (currentText.length > 0) {
                     term.write("\b \b");
                     currentText = currentText.substring(0, currentText.length - 1);
+                    cursorPosition -= 1;
                 }
             } else if (x.key != '\x1B[A' && x.key != '\x1B[B' && x.key != '\x1B[C' && x.key != '\x1B[D') {
                 term.write(x.key);
                 currentText += x.key;
+                cursorPosition += 1;
             } else if (x.key == '\x1B[D' && cursorPosition > currentText.length) {
                 term.write(x.key);
+                cursorPosition -= 1;
             }
             console.log(x);
         });
-    }, 150);
+    // }, 150);
 }
